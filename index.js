cc09949c70d4fd6274de99433f1c338ce36bd809
Aditya Ekawade <aditya.ekawade@gmail.com>
2019-12-11 23:21:33 -0800

    cli tool

---
+++ index.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const pdf = require('html-pdf');
+const pdfTemplate = require('./documents');
+
+
+const args = process.argv.slice(2)
+var fileName = args[0];
+
+fs.readFile(fileName, 'utf-8', (error, data) => {
+  if (error) throw error;
+  createPDF(JSON.parse(data));
+});
+
+
+function createPDF(data){
+  pdf.create(pdfTemplate(data), {"orientation": "potrait", "border": {"top":"0.5in", "bottom": "0.5in"}}).toFile('result.pdf', (err) => {
+  });
+}
